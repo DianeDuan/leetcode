691a9f976ec75511d14e24c7db1f273cf3cba441
ou.duan <ou.duan@qunar.com>
2016-01-06 09:23:12 +0800

    init project structure

---
+++ src/TestMain.java
@@ -0,0 +1,8 @@
+import algorithms.TwoSum_1;
+
+import java.lang.String;
+
+public class TestMain {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/algorithms/TwoSum_1.java
@@ -0,0 +1,7 @@
+package algorithms;
+
+public class TwoSum_1 {
+    public int[] twoSum(int[] nums, int target) {
+        return null;
+    }
+}
