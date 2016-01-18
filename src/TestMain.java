fb14a63d492914c056c213f30e4963bd58c29a1c
DianeDuan <duanou1992@163.com>
2016-01-18 09:55:28 +0800

    max depth of binary tree

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,6 +1,12 @@
-import algorithm.easy.AddDigits_258;
+import algorithm.easy.MaximumDepthOfBinaryTree_104;
+import algorithm.easy.TreeNode;
 
 public class TestMain {
     public static void main(String[] args) {
+        /*MaximumDepthOfBinaryTree_104 obj = new MaximumDepthOfBinaryTree_104();
+        TreeNode root = new TreeNode(0);
+        TreeNode left = new TreeNode(1);
+        TreeNode right = new TreeNode(2);
+        System.out.println(obj.maxDepth(root));*/
     }
 }
