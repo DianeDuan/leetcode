248a86309bc7e0ee8709826137cbe63a12b3b01b
DianeDuan <duanou1992@163.com>
2016-01-21 09:06:13 +0800

    no.122: best time to buy and sell stock ii

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,9 +1,4 @@
-import algorithm.medium.BestTimeToBuyAndSellStock_121;
-
 public class TestMain {
     public static void main(String[] args) {
-        BestTimeToBuyAndSellStock_121 obj = new BestTimeToBuyAndSellStock_121();
-        int[] prices = {1, 2, 3, 4};
-        System.out.println(obj.maxProfit(prices));
     }
 }
