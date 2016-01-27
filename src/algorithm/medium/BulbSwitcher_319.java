8be6fd2f6ab237031c47b9899ffa3aceb9842c5e
DianeDuan <duanou1992@163.com>
2016-01-27 09:48:33 +0800

    no.319: bulb switchers

---
+++ src/algorithm/medium/BulbSwitcher_319.java
@@ -0,0 +1,19 @@
+package algorithm.medium;
+
+/**
+ * Problem:
+ * https://leetcode.com/problems/bulb-switcher/
+ * <p/>
+ * Analysis:
+ * Except square numbers, other integers will be switch even times,
+ * because if the number k = a * b, and a != b, there must be k = b * a,
+ * that means the bulk no.k will be switched in the a round and the b round,
+ * that makes only square numbers will be switched odd times.
+ * In the end, only the bulks in square numbers position will be on.
+ * That changes this problem to find how many square numbers are there in the range of 1 to n.
+ */
+public class BulbSwitcher_319 {
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
+    }
+}
