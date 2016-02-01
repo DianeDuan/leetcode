b808eb7d5c5368e2e1c2409f0a0dfe6c2f6ef9ea
DianeDuan <duanou1992@163.com>
2016-02-01 16:25:05 +0800

    no.232: implement queue using stacks

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,4 +1,13 @@
+import algorithm.easy.ImplementQueueUsingStacks_232;
+
 public class TestMain {
     public static void main(String[] args) {
+        ImplementQueueUsingStacks_232 queue = new ImplementQueueUsingStacks_232();
+        queue.push(1);
+        queue.push(2);
+        System.out.println(queue.peek());
+        queue.pop();
+        queue.pop();
+        System.out.println(queue.empty());
     }
 }
---
+++ src/algorithm/easy/ImplementQueueUsingStacks_232.java
@@ -0,0 +1,40 @@
+package algorithm.easy;
+
+import java.util.Stack;
+
+/**
+ * Problem:
+ * https://leetcode.com/problems/implement-queue-using-stacks/
+ * <p/>
+ * Analysis:
+ * Put the last in element in the bottom of stack.
+ */
+public class ImplementQueueUsingStacks_232 {
+    Stack<Integer> queue = new Stack<Integer>();
+    Stack<Integer> inner = new Stack<Integer>();
+    // Push element x to the back of queue.
+    public void push(int x) {
+        while (!queue.empty()) {
+            inner.push(queue.pop());
+        }
+        queue.push(x);
+        while (!inner.empty()) {
+            queue.push(inner.pop());
+        }
+    }
+
+    // Removes the element from in front of queue.
+    public void pop() {
+        queue.pop();
+    }
+
+    // Get the front element.
+    public int peek() {
+        return queue.peek();
+    }
+
+    // Return whether the queue is empty.
+    public boolean empty() {
+        return queue.empty();
+    }
+}
