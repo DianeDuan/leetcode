ad3e83c7eb78c978634b5633dbcb744a2fcfee7f
DianeDuan <duanou1992@163.com>
2016-02-03 15:53:57 +0800

    no.203: remove linked list elements

---
+++ src/algorithm/easy/RemoveLinkedListElements_203.java
@@ -0,0 +1,36 @@
+package algorithm.easy;
+
+import algorithm.vo.ListNode;
+
+/**
+ * Problem:
+ * https://leetcode.com/problems/remove-linked-list-elements/
+ * <p/>
+ * Analysis:
+ * First remove the nodes' whose value are val in the head part of the list,
+ * then continue to use previous pointer and current pointer to traverse the list and remove elements.
+ */
+public class RemoveLinkedListElements_203 {
+    public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode current = head.next;
+        ListNode previous = head;
+        while (current != null) {
+            if (current.val == val) {
+                previous.next = current.next;
+                current = current.next;
+            } else {
+                current = current.next;
+                previous = previous.next;
+            }
+        }
+        return head;
+    }
+}
