21269577e4ed106ae4f39d89b4c4bdc325a6965b
ou.duan <duanou1992@163.com>
2016-02-14 17:31:52 +0800

    no.54: spiral matrix

---
+++ src/algorithm/medium/SpiralMatrixII_59.java
@@ -0,0 +1,13 @@
+package algorithm.medium;
+
+/**
+ * Problem:
+ * https://leetcode.com/problems/spiral-matrix-ii/
+ * <p/>
+ * Analysis:
+ */
+public class SpiralMatrixII_59 {
+    public int[][] generateMatrix(int n) {
+        return null;
+    }
+}
---
+++ src/algorithm/medium/SpiralMatrix_54.java
@@ -0,0 +1,50 @@
+package algorithm.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Problem:
+ * https://leetcode.com/problems/spiral-matrix/
+ * <p/>
+ * Analysis:
+ * Simulate the spiral traversal of the matrix.
+ */
+public class SpiralMatrix_54 {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return new ArrayList<Integer>();
+        }
+
+        List<Integer> result = new ArrayList<Integer>();
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+        while ((top <= bottom) && (left <= right)) {
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+            for (int i = right; i >= left; i--) {
+                if (bottom < top) {//avoid traverse the same row twice
+                    break;
+                }
+                result.add(matrix[bottom][i]);
+            }
+            bottom--;
+            for (int i = bottom; i >= top; i--) {
+                if (left > right) {//avoid traverse the same column twice
+                    break;
+                }
+                result.add(matrix[i][left]);
+            }
+            left++;
+        }
+        return result;
+    }
+}
