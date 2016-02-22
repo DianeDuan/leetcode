ee30d518956760a9cc4954e7eb80f95f43dfedc5
ou.duan <duanou1992@163.com>
2016-02-22 10:52:42 +0800

    no.173: binary search tree iterator

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,5 +1,3 @@
-import algorithm.medium.GameOfLife_289;
-
 public class TestMain {
     public static void main(String[] args) {
     }
---
+++ src/algorithm/medium/BinarySearchTreeIterator_173.java
@@ -0,0 +1,43 @@
+package algorithm.medium;
+
+import algorithm.vo.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * Problem:
+ * https://leetcode.com/problems/binary-search-tree-iterator/
+ * <p/>
+ * Analysis:
+ */
+public class BinarySearchTreeIterator_173 {
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+
+    public BinarySearchTreeIterator_173(TreeNode root) {
+        TreeNode current = root;
+        while (current != null) {
+            stack.push(current);
+            current = current.left;
+        }
+    }
+
+    /**
+     * @return whether we have a next smallest number
+     */
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    /**
+     * @return the next smallest number
+     */
+    public int next() {
+        TreeNode node = stack.pop();
+        TreeNode current = node.right;
+        while (current != null) {
+            stack.push(current);
+            current = current.left;
+        }
+        return node.val;
+    }
+}
