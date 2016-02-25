35a83409d78c2592ee8263c38d62d0ab5f7e5be2
ou.duan <duanou1992@163.com>
2016-02-25 10:58:34 +0800

    no.90: subsets ii

---
+++ src/algorithm/medium/SubsetsII_90.java
@@ -0,0 +1,44 @@
+package algorithm.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Problem:
+ * https://leetcode.com/problems/subsets-ii/
+ * <p/>
+ * Analysis:
+ * Skipping duplicates if it's not the start to avoid duplicate subsets.
+ */
+public class SubsetsII_90 {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return new ArrayList<List<Integer>>();
+        }
+
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        for (int i = 0; i <= nums.length; i++) {
+            result.addAll(getSubsets(nums, i, 0, new ArrayList<Integer>()));
+        }
+        return result;
+    }
+
+    private List<List<Integer>> getSubsets(int[] nums, int subsetSize, int start, List<Integer> current) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (current.size() == subsetSize) {
+            result.add(new ArrayList<Integer>(current));
+        } else {
+            for (int i = start; i < nums.length; i++) {
+                if (i > start && nums[i - 1] == nums[i]) {
+                    continue;
+                }
+                current.add(nums[i]);
+                result.addAll(getSubsets(nums, subsetSize, i + 1, current));
+                current.remove(current.size() - 1);
+            }
+        }
+        return result;
+    }
+}
--- target/classes/TODO.md
+++ target/classes/TODO.md
@@ -7,7 +7,7 @@ Verify Preorder Serialization Of a Binary Tree
 --------------
 
 unlocked:
-medium 82
+medium 80
 hard   51
 
 ---------------
