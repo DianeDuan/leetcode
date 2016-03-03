2e1aa18a1d216a48886e1de4f3f1c7ac34507986
ou.duan <duanou1992@163.com>
2016-03-03 16:00:23 +0800

    no.65: valid number

--- src/TODO.md
+++ src/TODO.md
@@ -3,18 +3,3 @@ N-Queens
 N-Queens II
 Unique Binary Search Tree II
 Verify Preorder Serialization Of a Binary Tree
-
---------------
-
-unlocked:
-medium 74
-hard   48
-
----------------
-
-locked:
-easy   12
-medium 29
-hard   12
-
----------------
---
+++ src/algorithm/hard/ValidNumber_65.java
@@ -0,0 +1,29 @@
+package algorithm.hard;
+
+/**
+ * Problem:
+ * https://leetcode.com/problems/valid-number/
+ * <p/>
+ * Analysis:
+ * Use regular expression.
+ * <p/>
+ * These are numbers:
+ * "-1."
+ * "+.8"
+ * <p/>
+ * These are not numbers:
+ * ".."
+ * "1e."
+ * <p/>
+ * Reference:
+ * http://www.cnblogs.com/springfor/p/3893598.html
+ */
+public class ValidNumber_65 {
+    public boolean isNumber(String s) {
+        if (s.trim().isEmpty()) {
+            return false;
+        }
+        String regex = "[-+]?(\\d+\\.?|\\.\\d+)\\d*(e[-+]?\\d+)?";
+        return s.trim().matches(regex);
+    }
+}
