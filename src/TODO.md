30e5fb88c04056b1b2798c95ef59d502e2c6fc6e
ou.duan <duanou1992@163.com>
2016-03-04 14:17:44 +0800

    no.113: path sum ii

--- src/TODO.md
+++
@@ -1,5 +0,0 @@
-Contains Duplicate III
-N-Queens
-N-Queens II
-Unique Binary Search Tree II
-Verify Preorder Serialization Of a Binary Tree
---
+++ src/algorithm/medium/PathSumII_113.java
@@ -0,0 +1,53 @@
+package algorithm.medium;
+
+import algorithm.vo.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Problem:
+ * https://leetcode.com/problems/path-sum-ii/
+ * <p/>
+ * Analysis:
+ * Preorder traverse binary tree and check if paths' sums.
+ */
+public class PathSumII_113 {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        return getPaths(root, sum, new ArrayList<Integer>());
+    }
+
+    private List<List<Integer>> getPaths(TreeNode root, int sum, List<Integer> current) {
+        if (root == null) {
+            return new ArrayList<List<Integer>>();
+        }
+
+        current.add(root.val);
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if (root.left == null && root.right == null) {
+            if (sum == sum(current)) {
+                result.add(current);
+            }
+        } else {
+            if (root.left != null) {
+                result.addAll(getPaths(root.left, sum, new ArrayList<Integer>(current)));
+            }
+            if (root.right != null) {
+                result.addAll(getPaths(root.right, sum, new ArrayList<Integer>(current)));
+            }
+        }
+        return result;
+    }
+
+    private int sum(List<Integer> nums) {
+        if (nums == null) {
+            return 0;
+        }
+
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        return sum;
+    }
+}
