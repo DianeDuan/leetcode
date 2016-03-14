7866e43bd4be40a4c6591ecc69c9f95841cca7ff
ou.duan <duanou1992@163.com>
2016-03-14 09:26:40 +0800

    no.79: word search

--- src/algorithm/medium/WordSearch_79.java
+++ src/algorithm/medium/WordSearch_79.java
@@ -5,10 +5,46 @@ package algorithm.medium;
  * https://leetcode.com/problems/word-search/
  * <p/>
  * Analysis:
+ * Recursive backtrace solution.
+ * <p/>
+ * Reference:
+ * http://www.cnblogs.com/springfor/p/3883942.html
  */
 public class WordSearch_79 {
     public boolean exist(char[][] board, String word) {
-        // TODO: 3/13/16  
+        if (board == null || word == null) {
+            return false;
+        }
+
+        int rowCount = board.length;
+        int columnCount = board[0].length;
+        for (int i = 0; i < rowCount; i++) {
+            for (int j = 0; j < columnCount; j++) {
+                if (isExist(board, i, j, word, 0)) {
+                    return true;
+                }
+            }
+        }
         return false;
+    }
+
+    private boolean isExist(char[][] board, int row, int column, String word, int pos) {
+        if (pos >= word.length()) {
+            return true;
+        }
+        if (row < 0 || row >= board.length || column < 0 || column >= board[0].length) {
+            return false;
+        }
+        if (word.charAt(pos) != board[row][column]) {
+            return false;
+        }
+
+        board[row][column] = '#';
+        boolean result = isExist(board, row + 1, column, word, pos + 1)
+                || isExist(board, row - 1, column, word, pos + 1)
+                || isExist(board, row, column + 1, word, pos + 1)
+                || isExist(board, row, column - 1, word, pos + 1);
+        board[row][column] = word.charAt(pos);
+        return result;
     }
 }
