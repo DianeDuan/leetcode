cb78983c0baaf676fa9ac9df84fc9c49d96671a9
ou.duan <duanou1992@163.com>
2016-03-21 09:03:48 +0800

    no.25: reverse nodes in k group

--- src/algorithm/hard/ReverseNodesInKGroup.java
+++ src/algorithm/hard/ReverseNodesInKGroup.java
@@ -8,45 +8,51 @@ import algorithm.vo.ListNode;
  * Link: https://leetcode.com/problems/reverse-nodes-in-k-group/
  * <p/>
  * Analysis:
+ * Traverse the list, record the head and the tail of every group,
+ * and revere the nodes between them.
+ * Add every reversed group to a new list.
+ * If the count of nodes in the last group is less than k,
+ * add the last group to the new list without reversion.
  */
 public class ReverseNodesInKGroup {
     public ListNode reverseKGroup(ListNode head, int k) {
-        if (head == null) {
-            return null;
+        if (head == null || k <= 0) {
+            return head;
         }
 
         ListNode dummyHead = new ListNode(0);
+        ListNode newTail = dummyHead;
         ListNode currentHead = head;
-        ListNode currentTail;
         ListNode current = head;
-        int count = 1;
+        int count = 0;
         while (current != null) {
-            if (count == k) {
-                currentTail = current;
-                currentTail.next = dummyHead.next;
-                dummyHead.next = reverse(currentHead, currentTail);
+            count++;
+            if (count != k) {
                 current = current.next;
-                currentHead = current;
-                count = 2;
             } else {
-                count++;
-                current = current.next;
+                ListNode next = current.next;
+                newTail.next = reverse(currentHead, next);
+                newTail = currentHead;
+                current = next;
+                currentHead = current;
+                count = 0;
             }
         }
         if (count > 0) {//the count of elements in the last group is less than k
+            newTail.next = currentHead;
         }
         return dummyHead.next;
     }
 
     private ListNode reverse(ListNode head, ListNode tail) {
-        if (head == null || tail == null) {
+        if (head == null) {
             return null;
         }
 
         ListNode previous = head;
         ListNode current = head.next;
         previous.next = null;
-        while (current != null) {
+        while (current != tail) {
             ListNode tmp = current.next;
             current.next = previous;
             previous = current;
