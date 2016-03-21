cf145a2ab1b09d9892e4d678c9a7068a0480530c
ou.duan <duanou1992@163.com>
2016-03-21 16:39:23 +0800

    no.17: letter combinations of a phone number

---
+++ src/algorithm/medium/LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,51 @@
+package algorithm.medium;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Problem:
+ * No: 17
+ * Link: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+ * <p/>
+ * Analysis:
+ * Recursive backtrace solution.
+ */
+public class LetterCombinationsOfAPhoneNumber {
+    private static Map<Character, char[]> keyboardMapping = initKeyboardMapping();
+
+    public List<String> letterCombinations(String digits) {
+        if (digits == null || digits.length() == 0) {
+            return new ArrayList<String>();
+        }
+
+        return combine(digits.toCharArray(), "");
+    }
+
+    private List<String> combine(char[] digits, String current) {
+        List<String> result = new ArrayList<String>();
+        if (current.length() == digits.length) {
+            result.add(current);
+        } else {
+            for (char c : keyboardMapping.get(digits[current.length()])) {
+                result.addAll(combine(digits, current + c));
+            }
+        }
+        return result;
+    }
+
+    private static Map<Character, char[]> initKeyboardMapping() {
+        Map<Character, char[]> result = new HashMap<Character, char[]>();
+        result.put('2', new char[]{'a', 'b', 'c'});
+        result.put('3', new char[]{'d', 'e', 'f'});
+        result.put('4', new char[]{'g', 'h', 'i'});
+        result.put('5', new char[]{'j', 'k', 'l'});
+        result.put('6', new char[]{'m', 'n', 'o'});
+        result.put('7', new char[]{'p', 'q', 'r', 's'});
+        result.put('8', new char[]{'t', 'u', 'v'});
+        result.put('9', new char[]{'w', 'x', 'y', 'z'});
+        return result;
+    }
+}
