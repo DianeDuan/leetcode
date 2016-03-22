3183b9d8e34047c1f8b25c287619b971c064db82
ou.duan <duanou1992@163.com>
2016-03-22 17:40:32 +0800

    no.71: simplify path

---
+++ src/algorithm/medium/SimplifyPath.java
@@ -0,0 +1,44 @@
+package algorithm.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Problem:
+ * No: 71
+ * Link: https://leetcode.com/problems/simplify-path/
+ * <p/>
+ * Analysis:
+ * Mock the process.
+ */
+public class SimplifyPath {
+    public String simplifyPath(String path) {
+        if (path == null || path.length() == 0) {
+            return "";
+        }
+
+        List<String> directoryHierarchies = new ArrayList<String>();
+        String[] directories = path.split("/");
+        for (String directory : directories) {
+            if (!directory.equals(".") && !directory.equals("..") && directory.length() > 0) {
+                directoryHierarchies.add(directory);
+            } else if (directory.equals("..") && directoryHierarchies.size() > 0) {
+                directoryHierarchies.remove(directoryHierarchies.size() - 1);
+            }
+        }
+        return buildPath(directoryHierarchies);
+    }
+
+    private String buildPath(List<String> directoryHierarchies) {
+        if (directoryHierarchies == null || directoryHierarchies.size() == 0) {
+            return "/";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String directory : directoryHierarchies) {
+            sb.append("/");
+            sb.append(directory);
+        }
+        return sb.toString();
+    }
+}
