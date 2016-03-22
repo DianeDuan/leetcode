5b649b9bc1085b13a4cb5e22a7d3e4603d3a9628
ou.duan <duanou1992@163.com>
2016-03-22 16:15:44 +0800

    no.57: insert interval

---
+++ src/algorithm/hard/InsertInterval.java
@@ -0,0 +1,43 @@
+package algorithm.hard;
+
+import algorithm.vo.Interval;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Problem:
+ * No: 57
+ * Link: https://leetcode.com/problems/insert-interval/
+ * <p/>
+ * Analysis:
+ * Traver the internals list.
+ * Add non-overlapping interval to the result directly.
+ * Merge overlapping interval and add merged interval at last.
+ * <p/>
+ * Reference:
+ * http://www.jiuzhang.com/solutions/insert-interval/
+ */
+public class InsertInterval {
+    public List<Interval> insert(List<Interval> intervals, Interval newInterval) {
+        if (newInterval == null || intervals == null) {
+            return intervals;
+        }
+
+        List<Interval> results = new ArrayList<Interval>();
+        int insertPos = 0;
+        for (Interval interval : intervals) {
+            if (interval.end < newInterval.start) {//current interval is before newInterval
+                results.add(interval);
+                insertPos++;
+            } else if (interval.start > newInterval.end) {//current interval is after newInterval
+                results.add(interval);
+            } else {//current interval overlaps newInterval
+                newInterval.start = Math.min(interval.start, newInterval.start);
+                newInterval.end = Math.max(interval.end, newInterval.end);
+            }
+        }
+        results.add(insertPos, newInterval);
+        return results;
+    }
+}
