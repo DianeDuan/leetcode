e6c5e6d487fdcfa02fdc566f06fd1adead1cde83
ou.duan <duanou1992@163.com>
2016-03-23 12:53:00 +0800

    no.28 & no.152

---
+++ src/algorithm/medium/DivideTwoIntegers.java
@@ -0,0 +1,55 @@
+package algorithm.medium;
+
+/**
+ * Problem:
+ * No: 29
+ * Link: https://leetcode.com/problems/divide-two-integers/
+ * <p/>
+ * Analysis:
+ * Use bit operations.
+ * <p/>
+ * Reference:
+ * http://www.acmerblog.com/leetcode-divide-two-integers-5977.html
+ * http://blog.csdn.net/linhuanmars/article/details/20024907
+ */
+public class DivideTwoIntegers {
+    public int divide(int dividend, int divisor) {
+        if (divisor == 0) {
+            return Integer.MIN_VALUE;
+        }
+
+        boolean isNegative = false;
+        if ((dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0)) {
+            isNegative = true;
+        }
+
+        long longDividend = Math.abs((long) dividend);
+        long longDivisor = Math.abs((long) divisor);
+
+        if (longDivisor > longDividend) {
+            return 0;
+        }
+
+        long d = longDivisor;
+        long bitCnt = 1;
+        long result = 0;
+        while (d < longDividend) {
+            d <<= 1;
+            bitCnt <<= 1;
+        }
+        while (d >= longDivisor) {
+            while (longDividend >= d) {
+                longDividend -= d;
+                result += bitCnt;
+            }
+            d >>= 1;
+            bitCnt >>= 1;
+        }
+
+        if (!isNegative && (result - 1) == Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        } else {
+            return (int) (isNegative ? -result : result);
+        }
+    }
+}
---
+++ src/algorithm/medium/MaximumProductSubarray.java
@@ -0,0 +1,41 @@
+package algorithm.medium;
+
+/**
+ * Problem:
+ * No: 152
+ * Link: https://leetcode.com/problems/maximum-product-subarray/
+ * <p/>
+ * Analysis:
+ * In the maximum sum subarray problem,
+ * when meet a negative element in the traversal,
+ * just set current sum 0,
+ * because a negative can only make the subarray sum smaller.
+ * But in this problem,
+ * when meet a negative element in the traversal,
+ * as two negative numbers's product is a positive number,
+ * it could be part of the maximum product subarray,
+ * so we can't just skip it.
+ * So in this problem,
+ * two variables are used to record the local maximum and the local minimum respectively.
+ * <p/>
+ * Reference:
+ * http://blog.csdn.net/linhuanmars/article/details/39537283
+ */
+public class MaximumProductSubarray {
+    public int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int localMax = nums[0];
+        int localMin = nums[0];
+        int global = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int tmp = localMax;
+            localMax = Math.max(Math.max(nums[i], localMax * nums[i]), localMin * nums[i]);
+            localMin = Math.min(Math.min(nums[i], tmp * nums[i]), localMin * nums[i]);
+            global = Math.max(global, localMax);
+        }
+        return global;
+    }
+}
