5d1830f74d0a3e54248d94463f8b0821801fa972
ou.duan <duanou1992@163.com>
2016-03-25 15:51:36 +0800

    no.117: populating next right pointers in each node ii

---
+++ src/algorithm/hard/PopulatingNextRightPointersInEachNodeII.java
@@ -0,0 +1,38 @@
+package algorithm.hard;
+
+import algorithm.vo.TreeLinkNode;
+
+/**
+ * Problem:
+ * No: 117
+ * Link: https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
+ * <p/>
+ * Analysis:
+ * <p/>
+ * Reference:
+ * https://gist.github.com/benjaminwu7/4702357
+ */
+public class PopulatingNextRightPointersInEachNodeII {
+    public void connect(TreeLinkNode root) {
+        while (root != null) {
+            TreeLinkNode current = root;
+            while (current != null) {
+                if (current.left != null && current.right != null) {
+                    current.left.next = current.right;
+                    current.right.next = getNextLevelFirst(current.next);
+                } else if (current.left != null || current.right != null) {
+                    getNextLevelFirst(current).next = getNextLevelFirst(current.next);
+                }
+                current = current.next;
+            }
+            root = getNextLevelFirst(root);
+        }
+    }
+
+    public TreeLinkNode getNextLevelFirst(TreeLinkNode root) {
+        if (root == null) return null;
+        else if (root.left != null) return root.left;
+        else if (root.right != null) return root.right;
+        else return getNextLevelFirst(root.next);
+    }
+}
