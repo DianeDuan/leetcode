1a2af59eb191087e12cb476887b67484435e8fc2
DianeDuan <duanou1992@163.com>
2016-08-02 10:17:24 +0800

    no.349: intersection of two arrays

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,3 +1,5 @@
+import algorithm.easy.IntersectionOfTwoArrays;
+
 public class TestMain {
     public static void main(String[] args) {
     }
---
+++ src/algorithm/easy/IntersectionOfTwoArrays.java
@@ -0,0 +1,46 @@
+package algorithm.easy;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Problem:
+ * No: 349
+ * Link: https://leetcode.com/problems/intersection-of-two-arrays/
+ *
+ * Analysis:
+ * Sort two arrays and then compare two sorted array to get their intersection.
+ */
+public class IntersectionOfTwoArrays {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return null;
+        }
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        Set<Integer> intersection = new HashSet<Integer>();
+        int i = 0,j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                intersection.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        int[] result = new int[intersection.size()];
+        i = 0;
+        for (Integer item : intersection) {
+            result[i] = item;
+            i++;
+        }
+        return result;
+    }
+}
