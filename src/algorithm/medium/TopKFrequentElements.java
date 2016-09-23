46a1e9ff63136cd9b06ab7b908c983b224382a43
DianeDuan <duanou1992@163.com>
2016-09-23 14:08:36 +0800

    tmp

---
+++ src/algorithm/medium/TopKFrequentElements.java
@@ -0,0 +1,51 @@
+package algorithm.medium;
+
+import jdk.nashorn.internal.ir.IdentNode;
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.util.*;
+
+/**
+ * Problem:
+ * No: 347
+ * Link: https://leetcode.com/problems/top-k-frequent-elements/
+ *
+ * Analysis:
+ */
+public class TopKFrequentElements {
+    // TODO: 8/2/16  
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return new ArrayList<Integer>();
+        }
+
+
+
+        Map<Integer, Integer> frequency = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int key = nums[i];
+            if (frequency.containsKey(key)) {
+                int count = frequency.get(key);
+                frequency.put(key, count + 1);
+            } else {
+                frequency.put(key, 1);
+            }
+        }
+
+        Set<Integer> result = new HashSet<Integer>();
+        int minFrequencyNum = nums[0];
+        int minFrequency = frequency.get(nums[0]);
+        result.add(nums[0]);
+        for (Map.Entry<Integer, Integer> entry : frequency.entrySet()) {
+            if (entry.getValue() > minFrequency) {
+                result.add(minFrequency);
+            } else {
+                result.add(minFrequency);
+                result.remove(minFrequencyNum);
+                minFrequency = entry.getValue();
+                minFrequencyNum = entry.getValue();
+            }
+        }
+        return new ArrayList<Integer>(result);
+    }
+}
