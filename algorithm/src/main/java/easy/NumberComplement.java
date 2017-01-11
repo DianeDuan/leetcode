1bacffcc0a3872e1001a6eaf38cbfe5a6dda60b0
Diane <duanou1992@163.com>
2017-01-11 10:08:54 +0800

    no.476: Number Complement

---
+++ algorithm/src/main/java/easy/NumberComplement.java
@@ -0,0 +1,21 @@
+package easy;
+
+/**
+ * Problem:
+ * No: 476
+ * Link: https://leetcode.com/problems/number-complement/
+ *
+ * Analysis:
+ * Use xor operation to get complement
+ */
+public class NumberComplement {
+    public int findComplement(int num) {
+        int numBitCount = Integer.toBinaryString(num).length();
+        int val = 0;
+        for (int i = 0; i < numBitCount; i++) {
+            val <<= 1;
+            val += 1;
+        }
+        return val ^ num;
+    }
+}
